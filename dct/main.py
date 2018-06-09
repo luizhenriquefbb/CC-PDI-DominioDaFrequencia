cc364669f9b9b9fd72a27822a276005671875145
Luiz Henrique Freire Barros <luizhenriquefbb@gmail.com>
2018-06-09 14:22:22 -0300

    selecting important pixels

--- dct/main.py
+++ dct/main.py
@@ -6,14 +6,16 @@ from numpy import uint8, array
 
 from dct import dct2D
 from idct import idct2D
+from selectPixels import selectImportantPixels
 
 def main():
     parser = argparse.ArgumentParser(description='Discrete Cosine Transform')
     parser.add_argument('-i', '--input', action='store')
     parser.add_argument('-o', '--output', action='store')
+    parser.add_argument('-n', '--pixelNumber', action='store')
     args = parser.parse_args()
 
-    if args.input is None or args.output is None:
+    if args.input is None or args.output is None or args.pixelNumber is None:
         parser.print_help()
         exit(2)
 
@@ -22,11 +24,16 @@ def main():
     dtc2d_ret = dct2D(img)
     idtc2d_ret = idct2D(dtc2d_ret)
 
+    
+    important = selectImportantPixels(dtc2d_ret, n=args.pixelNumber)
+    
     cv2.imwrite(args.output, dtc2d_ret)
+    cv2.imwrite(args.output+"-important.jpg", important)
 
     cv2.imshow("Original", img)
     cv2.imshow("DCT Result", dtc2d_ret.astype(uint8))
     cv2.imshow("IDCT Result", idtc2d_ret.astype(uint8))
+    cv2.imshow("important pixels", important.astype(uint8))
     cv2.waitKey(0)
 
 if __name__ == '__main__':
---
+++ dct/selectPixels.py
@@ -0,0 +1,63 @@
+'''
+
+'''
+
+import numpy as np
+from math import sqrt
+
+def selectImportantPixels(M, n = 1):
+    '''
+    Select important pixels from matrix
+
+    Parameters:
+        M - Matrix
+        n - number of pixels
+
+    return:
+        Matrix with most important pixels
+    '''
+    L,C = M.shape
+    result = np.zeros((L,C))
+    
+    importantPixels = _getPixelsList(M)[:int(n)]
+    
+
+    for pixel in importantPixels:
+        result[pixel.x][pixel.y] = pixel.grayScale
+
+    
+    return result
+
+
+def _getPixelsList(M):
+    '''
+    Return a sorted list of pixels
+    '''
+    L, C = M.shape
+
+    pixels = []
+    for i in range(L):
+        for j in range(C):
+            pixels.append(_Pixel(i,j,M[i][j]))
+
+    sortedList = sorted(pixels, key=lambda pixel: _getDistanceFromDC(pixel.x,pixel.y), reverse=False)
+    return sortedList
+
+def _getDistanceFromDC(x,y):
+    '''
+    Euclidian distance from 0,0
+
+    Paramans:
+        x,y - coordenates
+    '''
+
+    return sqrt(x**2 + y**2)
+
+class _Pixel(object):
+    def __init__(self, x, y, grayScale):
+        self.x = x
+        self.y = y
+        self.grayScale = grayScale
+
+    def __str__(self):
+        return "{ "+str(self.x) + str(self.y) + str(self.grayScale) + " } \n"
