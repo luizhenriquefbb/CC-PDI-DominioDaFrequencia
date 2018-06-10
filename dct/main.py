512dfe407cdca357559c33dd84d7f5160db35037
Wesnydy Ribeiro <wesnydy@gmail.com>
2018-06-09 22:15:53 -0300

    stable version for 3channel image

--- dct/main.py
+++ dct/main.py
@@ -2,10 +2,11 @@
 
 import argparse
 import cv2
-from numpy import uint8, array
+import numpy as np
 
-from dct import dct2D
-from idct import idct2D
+import dct
+import idct
+import imghandler
 
 def main():
     parser = argparse.ArgumentParser(description='Discrete Cosine Transform')
@@ -17,16 +18,33 @@ def main():
         parser.print_help()
         exit(2)
 
-    img = cv2.imread(args.input, cv2.IMREAD_GRAYSCALE)
-
-    dtc2d_ret = dct2D(img)
-    idtc2d_ret = idct2D(dtc2d_ret)
-
-    cv2.imwrite(args.output, dtc2d_ret)
+    img = cv2.imread(args.input)
+    pxlfmt = imghandler.get_pixel_format(img)
 
     cv2.imshow("Original", img)
-    cv2.imshow("DCT Result", dtc2d_ret.astype(uint8))
-    cv2.imshow("IDCT Result", idtc2d_ret.astype(uint8))
+
+    if pxlfmt == imghandler.YIQ_FMT:
+        grayimg = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+        dtc2d_ret = dct.dct2D(grayimg)
+        cv2.imshow("DCT Result", dtc2d_ret.astype(np.uint8))
+
+        idtc2d_ret = idct.idct2D(dtc2d_ret)
+        cv2.imshow("IDCT Result", idtc2d_ret.astype(np.uint8))
+
+    else:
+        r, g, b = imghandler.split_channels(img)
+
+        r_dtc2d_ret = dct.dct2D(r)
+        g_dtc2d_ret = dct.dct2D(g)
+        b_dtc2d_ret = dct.dct2D(b)
+        cv2.imshow("DCT Result", imghandler.merge_channels(r_dtc2d_ret, g_dtc2d_ret, b_dtc2d_ret))
+
+        r_idtc2d_ret = idct.idct2D(r_dtc2d_ret)
+        g_idtc2d_ret = idct.idct2D(g_dtc2d_ret)
+        b_idtc2d_ret = idct.idct2D(b_dtc2d_ret)
+        cv2.imshow("IDCT Result", imghandler.merge_channels(r_idtc2d_ret, g_idtc2d_ret, b_idtc2d_ret))
+
     cv2.waitKey(0)
 
 if __name__ == '__main__':
